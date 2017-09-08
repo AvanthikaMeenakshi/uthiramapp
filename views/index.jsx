fbcd78b0b66865a071f4386cacd0865bb0f212b1
avanthika <avanthika157@gmail.com>
2017-09-08 19:37:01 +0530

    Initial commit

--- views/index.jsx
+++
@@ -1,9 +0,0 @@
-var React = require('react');
-
-class HelloMessage extends React.Component {
-  render() {
-    return <div>Hello dfddffd{this.props.title}</div>;
-  }
-}
-
-module.exports = HelloMessage;
