af52555cc75b0128d4841182ad867d9a19535157
Avanthika Meenakshi <avanthika157@gmail.com>
2019-05-26 00:41:51 +0530

    Fixed issue with booting

--- frontend/src/components/Main/index.js
+++ frontend/src/components/Main/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Router, Route } from 'react-router';
 import HomePage from '../HomePage';
-import NotFound from '../NotFound';
+import PageNotFound from '../PageNotFound';
 import DonorRegistration from '../DonorRegistration';
 
 const Routes = props => (
   <Router {...props}>
     <Route path="/" component={HomePage} />
     <Route path="/register" component={DonorRegistration} />
-    <Route path="*" component={NotFound} />
+    <Route path="*" component={PageNotFound} />
   </Router>
 );
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { browserHistory } from 'react-router';
-import App from './components/App';
+import App from './components/Main';
 import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
 import 'bootstrap/dist/css/bootstrap.min.css';
